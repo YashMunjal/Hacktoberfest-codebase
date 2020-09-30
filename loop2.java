33f7bc5e0b4ae80b2095d1f19c07aef5d40800aa
Saksham Aggarwal <57211593+saksham-devil@users.noreply.github.com>
2020-09-30 23:07:36 +0530

    Create loop2.java

---
+++ loop2.java
@@ -0,0 +1,11 @@
+public class NestedForExample {  
+public static void main(String[] args) {  
+//loop of i  
+for(int i=1;i<=3;i++){  
+//loop of j  
+for(int j=1;j<=3;j++){  
+        System.out.println(i+" "+j);  
+}//end of i  
+}//end of j  
+}  
+}  

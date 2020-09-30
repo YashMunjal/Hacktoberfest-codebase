4e8353982623db06e8d6e5f3247d7155ba84e71d
Saksham Aggarwal <57211593+saksham-devil@users.noreply.github.com>
2020-09-30 23:12:27 +0530

    Create pyramid2.java

---
+++ pyramid2.java
@@ -0,0 +1,11 @@
+public class PyramidExample2 {  
+public static void main(String[] args) {  
+int term=6;  
+for(int i=1;i<=term;i++){  
+for(int j=term;j>=i;j--){  
+        System.out.print("* ");  
+}  
+System.out.println();//new line  
+}  
+}  
+}  
